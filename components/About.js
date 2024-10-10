ff11f187317369820e7e73391e39956330d79ecf
Naman Ahuja <96652306+Namanahuja82@users.noreply.github.com>
2024-10-11 02:04:36 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -126,7 +126,7 @@ const About = () => {
             Hi, I am Naman
           </h2>
           <a
-            href="/naman-ofc.pdf"
+            href="/naman-resume.pdf"
             download
             className="inline-block px-6 py-3 bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold text-base rounded-full shadow-lg hover:from-purple-600 hover:to-pink-600 transition duration-300 transform hover:scale-105 mb-8"
           >
